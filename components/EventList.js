e284e9a94e20f38243f043ccad953686aa97b195
David Sotto Mayor <ddsmmf@gmail.com>
2021-11-12 21:49:04 -0400

    Creating standart React components in Nextjs

---
+++ components/EventList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EventItem from './EventItem';
+
+const EventList = (props) => {
+    return (
+        <ul>
+            {props.items.map( item => <EventItem key={item.id} {...item} />)}
+        </ul>
+    )
+}
+
+export default EventList
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css';
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/EventList'
 
 export default function Home() {
+
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div className={styles.container}>
       <h1>Hello from NextJs!</h1>
+      <EventList items={featuredEvents} />
     </div>
   )
 }
