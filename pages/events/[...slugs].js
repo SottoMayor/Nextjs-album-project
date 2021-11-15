4980527c5d49836a74d96fdaa9752c55a1184008
David Sotto Mayor <ddsmmf@gmail.com>
2021-11-14 21:43:26 -0400

    Working with data in Events slug page

--- pages/events/[...slugs].js
+++ pages/events/[...slugs].js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { useRouter } from 'next/dist/client/router';
+import EventList from '../../components/events/EventList';
+
+import { getFilteredEvents } from '../../dummy-data';
 
 const EventsSlugs = () => {
 
     const router = useRouter();
-    const slugs = router.query.slugs;
-    console.log(slugs)
+    const filteredData = router.query.slugs;
+    
+    if(!filteredData){
+        return <p className="center">Loading... </p>
+    }
+
+    const year = +filteredData[0];
+    const month = +filteredData[1];
+
+    if(isNaN(year) || isNaN(month) || year < 2021 || year > 2030 || month < 0 || month > 12){
+        return <p className="center">Invalid filter! Adjust yout values!</p>
+    }
+
+    const filteredEvents = getFilteredEvents({year, month});
+
+    if(!filteredEvents || filteredEvents.length === 0){
+        return <p className="center">No events found to this filter! X.X</p>
+    }
 
     return (
         <div>
-            <h1>List of slugs: {slugs}</h1>
+            <EventList items={filteredEvents}/>
         </div>
     )
 }
